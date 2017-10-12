92c89e61d3e95fb7270babca131229cc95bc5d99
Сергей Черноудов <s.chernoudov@indatalab.ru>
2017-10-12 23:15:38 +0300

    Russian language

---
+++ upload/catalog/language/ru-ru/account/affiliate.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']             = 'Ваша партнерская информация';
+
+// Text
+$_['text_account']              = 'Личный кабинет';
+$_['text_affiliate']            = 'Партнерская учетная запись';
+$_['text_my_affiliate']         = 'Партнерская учетная запись';
+$_['text_payment']              = 'Информация об оплате';
+$_['text_cheque']               = 'Чек';
+$_['text_paypal']               = 'PayPal';
+$_['text_bank']                 = 'Банковский перевод';
+$_['text_success']              = 'Ваша учетная запись была успешно обновлена';
+$_['text_agree']                = 'Я прочитал и согласен с этим <a href="%s" class="agree"><b>%s</b></a>';
+
+// Entry
+$_['entry_company']             = 'Компания';
+$_['entry_website']             = 'Web-сайт';
+$_['entry_tax']                 = 'ИНН';
+$_['entry_payment']             = 'Способ оплаты';
+$_['entry_cheque']              = 'Cheque Payee Name';
+$_['entry_paypal']              = 'PayPal Email Account';
+$_['entry_bank_name']           = 'Bank Name';
+$_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
+$_['entry_bank_swift_code']     = 'SWIFT Code';
+$_['entry_bank_account_name']   = 'Account Name';
+$_['entry_bank_account_number'] = 'Account Number';
+
+// Error
+$_['error_agree']               = 'Warning: You must agree to the %s!';
+$_['error_cheque']              = 'Cheque Payee Name required!';
+$_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
+$_['error_bank_account_name']   = 'Account Name required!';
+$_['error_bank_account_number'] = 'Account Number required!';
+$_['error_custom_field']        = '%s required!';
